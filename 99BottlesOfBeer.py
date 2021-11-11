fdf26acf7e0d011b896aff88548e64a19389e35f
exedot <83253371+exedot@users.noreply.github.com>
2021-11-12 09:48:35 +1100

    Lots etcetera

--- 99BottlesOfBeer.py
+++ 99BottlesOfBeer.py
@@ -1,14 +1,27 @@
 def start():
-    beer = int(99)
-    bs = str(beer)
-    b = str(bs + " bottles of beer on the wall,\n" + bs + " bottles of beer.\nTake one down, pass it around\n" + bs + " bottles of beer on the wall")
-    while beer >=1: hasbeer = True
-
-    while hasbeer == True:
-        print(b)
-        beer = (beer - 1); return beer
-    
-    if hasbeer == False:
-        print("No more bottles of beer on the wall,\nno more bottles of beer.\nGo to the store and buy some more,\n99 bottles of beer on the wall...")
-
+    n = int(99)
+    for n in range(99,-1,-1):
+        if n>2:
+            print(str(n) + " bottles of beer on the wall,")
+            print(str(n) + " bottles of beer.")
+            print("Take one down, pass it around,")
+            print(str(n-1) +" bottles of beer on the wall.")
+            print("")
+        elif n==2:
+            print(str(n) +" bottles of beer on the wall,")
+            print(str(n) +" bottles of beer.")
+            print("Take one down, pass it around,")
+            print(str(n-1) +" bottle of beer on the wall.")
+            print(str(""))
+        elif n==1:
+            print(str(n) +" bottle of beer on the wall,")
+            print(str(n) +" bottle of beer.")
+            print("Take one down, pass it around,")
+            print("No more bottles of beer on the wall.")
+            print("")
+        else:
+            print("No more bottles of beer on the wall,")
+            print("no more bottles of beer.")
+            print("Go to the strore and buy some more,")
+            print("99 bottles of beer on the wall.")
 start()
---
+++ Caesar.py
@@ -0,0 +1,2 @@
+def start():
+    a="N", b="O", c="P", d="Q", e="R", f="S", g="T", h="U",i="V",j="W",k="X",l="Y",m="Z",n="A",o="B",p="C",q="D",r="E",s="F",t="G",u="H",v="I",w="J",x="K",y="L",z="M"
